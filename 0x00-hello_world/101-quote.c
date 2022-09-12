7b1120cb25c9432e2e6010ef3009ff071f801b6e
EzealaCodes <chrismacmum@gmail.com>
2022-09-11 20:40:25 -0700

    101 quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,14 @@
  * main - Prints the string without printf or puts functions
  *
  * Description: using the main function
- * this program prints "and that piece of art is useful" - Dora Korpar, 2015-19-19
+ * this program prints "and that piece of art is useful" - Dora Korpar, 
+ * 2015-10-19
  * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
+	write(2,
+              "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",
+	      59);
 	return (1);
 }
