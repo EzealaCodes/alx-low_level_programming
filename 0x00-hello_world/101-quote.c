29fbeef06ace6850c765390ab7558d0569a141cc
EzealaCodes <chrismacmum@gmail.com>
2022-09-12 17:54:34 -0700

    101 quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Prints the string without printf or puts functions
+ * main - Entry point
  *
- * Description: using the main function
- * this program prints "and that piece of art is useful" - Dora Korpar, 
- * 2015-10-19
- * Return: 1
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2,
-              "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",
-	      59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
