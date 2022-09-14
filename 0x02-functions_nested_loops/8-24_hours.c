d8608403a833633708ec7399861f01eda8cbf326
EzealaCodes <chrismacmum@gmail.com>
2022-09-14 16:21:06 -0700

    Jack Bauer 24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,8 @@
 #incude "main.h"
 
 /**
- * main - Write a function that prints every minute of the day
- *
- * n from 00:00 to 23:59, min loop counts mins...
- * Return: 0
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
