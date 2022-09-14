9519add0a3ab75bf1c4bf65b6b38a7892f3ec289
EzealaCodes <chrismacmum@gmail.com>
2022-09-13 19:38:38 -0700

    1 alaphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
 /**
- * main - check the code
+ * print alphabet function to print abc
  *
- * Return: Always 0.
+ * Retun: 0
  */
-int main(void)
+void print_alphabet(void)
 {
-	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
-
