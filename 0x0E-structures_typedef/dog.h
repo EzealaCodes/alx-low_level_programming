4756b31fded3808718ce5ec08093b845aed7d639
EzealaCodes <chrismacmum@gmail.com>
2022-10-11 20:41:23 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,31 +1,27 @@
-#include <stdio.h>
-
-#include "dog.h"
-
-
+#ifndef DOG_H
+#define DOG_H
 
 /**
-
- * main - check the code for Holberton School students.
-
- *
-
- * Return: Always 0.
-
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
-
-int main(void)
-
+struct dog
 {
+	char *name;
+	float age;
+	char *owner;
+};
 
-  struct dog my_dog;
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-
-  init_dog(&my_dog, "Django", 3.5, "Jay");
-
-  printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-
-  return (0);
-
-}
+#endif
