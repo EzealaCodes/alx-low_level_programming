9d3263eb53da03f9e6e5daa1f89883902cda7a69
EzealaCodes <chrismacmum@gmail.com>
2022-10-31 19:09:42 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,77 +1,38 @@
 #include "main.h"
 
-#include <stdlib.h>
-
-
-
 /**
-
- * read_textfile - Reads a text file and prints it to POSIX stdout.
-
- * @filename: A pointer to the name of the file.
-
- * @letters: The number of letters the
-
- *           function should read and print.
-
- *
-
- * Return: If the function fails or filename is NULL - 0.
-
- *         O/w - the actual number of bytes the function can read and print.
-
+ * read_textfile - reads a text file and prints it to the POSIX stdout
+ * @filename: filename
+ * @letters: numbers of letters it should read and print
+ * Description: if filename is NULL || write fails or does not
+ *      write the expected number of bytes return 0
+ * Return: actual number of letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	ssize_t o, r, w;
-
-	char *buffer;
-
-
-
-	if (filename == NULL)
-
+	if (!filename)
 		return (0);
 
+	fd = open(filename, O_RDONLY);
 
-
-	buffer = malloc(sizeof(char) * letters);
-
-	if (buffer == NULL)
-
+	if (fd == -1)
 		return (0);
 
-
-
-	o = open(filename, O_RDONLY);
-
-	r = read(o, buffer, letters);
-
-	w = write(STDOUT_FILENO, buffer, r);
-
-
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-
-	{
-
-		free(buffer);
-
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
 
-	}
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
+	close(fd);
 
+	free(buf);
 
-	free(buffer);
-
-	close(o);
-
-
-
-	return (w);
-
+	return (nwr);
 }
